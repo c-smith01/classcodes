9534863d7199c820738f3c14dc2f8f6138241643
c-smith01 <147568006+c-smith01@users.noreply.github.com>
2024-01-23 17:23:25 -0600

    Add files via upload
    First draft of code for NUEN 644 first homework

---
+++ SMITH_NUEN644_HW1.py
@@ -0,0 +1,50 @@
+'''
+Created by Coleman Smith on 1/23/24
+NUEN/MEEN 644 HW1
+Due 6 February 2024
+'''
+import numpy as np
+import matplotlib as mlp
+
+######## Problem #1 (a) ###########
+
+# Define constants and initial conditions
+l = 1 #m
+m = 1 #kg
+g = 9.81 #m/s^2
+theta_0 = np.pi/12
+theta_dot = 0
+
+# Initialize array of zeros for solution
+theta = np.zeros(6)
+
+# Deine ODE as a function
+def f(t,theta):
+    p = 1
+    return p
+
+def euler_expl(t,thetas):
+    theta[i + 1] = theta -(g/l)*np.sin(t)
+
+def euler_full_impl(t,thetas):
+    theta[i + 1] = theta -(g/l)*np.sin(t)
+
+def rk_sncd(t,thetas):
+    theta[i + 1] = theta -(g/l)*np.sin(t)
+
+def rk_frth(t,thetas):
+    theta[i + 1] = theta -(g/l)*np.sin(t)
+
+
+######## Problem #1 (b) ###########
+
+h = 0.05 #s
+tspan = np.arange(0,2 + h,h)
+print(tspan)
+
+######## Problem #1 (c) ###########
+
+######## Problem #1 (d) ###########
+
+######## Problem #1 (e) ###########
+
