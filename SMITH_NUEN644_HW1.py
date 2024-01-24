6e4223474e029fcad128d32293f4a1f58cdc36e4
c-smith01 <147568006+c-smith01@users.noreply.github.com>
2024-01-23 18:09:12 -0600

    Add files via upload
    Update copy of HW program

--- SMITH_NUEN644_HW1.py
+++ SMITH_NUEN644_HW1.py
@@ -6,7 +6,10 @@ Due 6 February 2024
 import numpy as np
 import matplotlib as plt
 
+
+###################################
 ######## Problem #1 (a) ###########
+###################################
 
 # Define constants and initial conditions
 l = 1 #m
@@ -18,13 +21,10 @@ theta_dot = 0
 # Initialize array of zeros for solution
 theta = np.zeros(6)
 
-# Deine ODE as a function
-def f(t,theta):
-    p = 1
-    return p
-
-def euler_expl(t,thetas):
-    theta[i + 1] = theta -(g/l)*np.sin(t)
+def euler_expl(h,ts,thetas):
+    theta_n = [theta_0]
+    for i in range(0,2/h):
+        theta_n[i+1] = theta[i] - (g/l)*np.sin(theta[i])
 
 def euler_full_impl(t,thetas):
     theta[i + 1] = theta -(g/l)*np.sin(t)
@@ -36,15 +36,20 @@ def rk_frth(t,thetas):
     theta[i + 1] = theta -(g/l)*np.sin(t)
 
 
+###################################
 ######## Problem #1 (b) ###########
+###################################
 
 h = 0.05 #s
 tspan = np.arange(0,2 + h,h)
 print(tspan)
 
-######## Problem #1 (c) ###########
 
-# Create scatter plot
+###################################
+######## Problem #1 (c) ###########
+###################################
+
+# Create scatter plot of angular displacement vs. time
 plt.scatter(x, y, color='blue', label='Random Data Points')
 
 # Add labels and title
@@ -58,7 +63,9 @@ plt.legend()
 # Display the plot
 plt.show()
 
+###################################
 ######## Problem #1 (d) ###########
+###################################
 
 # Create scatter plot
 plt.scatter(x, y, color='blue', label='Random Data Points')
