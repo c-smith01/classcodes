cf6dd797bbba14766263c783e54edab97af55404
c-smith01 <147568006+c-smith01@users.noreply.github.com>
2024-02-06 16:28:43 -0600

    updated methods for numerical methods

--- SMITH_NUEN644_HW1.py
+++ SMITH_NUEN644_HW1.py
@@ -43,10 +43,12 @@ def rk_scnd(stepsize,ts,thetas):
     theta_n[0] = thetas[0]
     dot_theta_n[0] = thetas[1]
     for i in range(1,len(ts)):
-        k1 = []
-        k2 = []
-        theta_n[i] = theta_n[i-1] + dot_theta_n[i-1]*stepsize - (g/l)*np.sin(theta_n[i-1])*(stepsize*stepsize)/2
-        dot_theta_n[i] = (theta_n[i-2]-theta_n[i-1])/stepsize + (g/l)*np.sin(dot_theta_n[i-2])*stepsize/2
+        k11 = dot_theta_n[i-1]
+        k12 = -(g/l)*np.sin(theta_n[i-1])
+        k21 = dot_theta_n[i-1] + k12*stepsize
+        k22 = -(g/l)*np.sin(theta_n[i-1]+stepsize*dot_theta_n[i-1])
+        theta_n[i] = theta_n[i-1] + (stepsize/2)*(k11+k21)
+        dot_theta_n[i] = dot_theta_n[i-1] + (stepsize/2)*(k12+k22)
     return theta_n, dot_theta_n
 
 def rk_frth(stepsize,ts,thetas):
@@ -55,12 +57,16 @@ def rk_frth(stepsize,ts,thetas):
     theta_n[0] = thetas[0]
     dot_theta_n[0] = thetas[1]
     for i in range(1,len(ts)):
-        k1 = []
-        k2 = []
-        k3 = []
-        k4 = []
-        theta_n[i] = theta_n[i-1] + dot_theta_n[i-1]*stepsize - (g/l)*np.sin(theta_n[i-1])*(stepsize*stepsize)/2
-        dot_theta_n[i] = (theta_n[i-2]-theta_n[i-1])/stepsize + (g/l)*np.sin(dot_theta_n[i-2])*stepsize/2
+        k11 = dot_theta_n[i-1]
+        k12 = -(g/l)*np.sin(theta_n[i-1])
+        k21 = (dot_theta_n[i-1] + k12*stepsize)/2
+        k22 = -(g/l)*np.sin(theta_n[i-1]+stepsize*dot_theta_n[i-1])
+        k31 = []
+        k32 = []
+        k41 = []
+        k42 = []
+        theta_n[i] = theta_n[i-1] + (stepsize/6)*(k11+2*k21+2*k31+k41)
+        dot_theta_n[i] = dot_theta_n[i-1] + (stepsize/6)*(k12+2*k22+2*k32+k42)
     return theta_n, dot_theta_n
 
 # Additional exact solution method for sanity check purposes
@@ -93,6 +99,9 @@ euler_esol = euler_expl(h,tspan,theta_init)
 euler_isol = euler_full_impl(h,tspan,theta_init)
 #print(euler_isol)
 
+rk2_sol = rk_scnd(h,tspan,theta_init)
+#print(rk2_sol)
+
 exact_sol = exact(h,tspan,theta_init)
 
 
@@ -104,6 +113,7 @@ exact_sol = exact(h,tspan,theta_init)
 # Create scatter plot of angular displacement vs. time
 plt.plot(tspan, euler_esol[0], color='blue', label='Explicit Euler')
 plt.plot(tspan, euler_isol[0], color='green', label='Implicit Euler')
+plt.plot(tspan, rk2_sol[0], color='black', label='2nd Order R-K')
 plt.plot(tspan, exact_sol[0], color='red', label='Exact')
 
 # Add labels and title
@@ -123,7 +133,8 @@ plt.show()
 
 # Create scatter plot
 plt.plot(tspan, euler_esol[1], color='blue', label='Explicit Euler')
-plt.plot(tspan, euler_isol[0], color='green', label='Implicit Euler')
+plt.plot(tspan, euler_isol[1], color='green', label='Implicit Euler')
+plt.plot(tspan, rk2_sol[1], color='black', label='2nd Order R-K')
 plt.plot(tspan, exact_sol[1], color='red', label='Exact')
 # Add labels and title
 plt.xlabel('Time [s]')
