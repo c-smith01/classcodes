717c7208c48c707a65ca66125db903ed8298afc3
c-smith01 <147568006+c-smith01@users.noreply.github.com>
2024-02-14 16:06:43 -0600

    Started new sections of code

--- SMITH_NUEN644_HW2.py
+++ SMITH_NUEN644_HW2.py
@@ -3,8 +3,29 @@ Created by Coleman Smith on 1/23/24
 NUEN/MEEN 644 HW1
 Due 15 February 2024
 '''
-L = 20
-k= 50
-beta = 100
-T_0 = 100
-T_inf = 30
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define constants
+L     = 20   # cm
+k     = 50   # W/m*C
+beta  = 100  # W/m^2*C
+T_0   = 100  # deg C
+T_inf = 30   # deg C
+q_in  = 10E3 # W/m
+
+n_ITCV = 10
+
+
+###################################
+######## Problem #1 (a) ###########
+###################################
+
+###################################
+######## Problem #2 (a) ###########
+###################################
+
+alpha_set = np.linspace(1, 1.8, 9)
+print(alpha_set)
+
+
