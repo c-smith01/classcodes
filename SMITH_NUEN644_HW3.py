0a4b6f597fbc6e0837f6334ddf3190253dddcc17
c-smith01 <65824615+cole-cmd@users.noreply.github.com>
2024-03-08 21:07:08 -0600

    added iterative method print for P1

--- SMITH_NUEN644_HW3.py
+++ SMITH_NUEN644_HW3.py
@@ -26,6 +26,7 @@ JTCV = ITCV
 ITMAX = ITCV + 2
 JTMAX = ITMAX
 Ts_dim = (ITMAX, ITMAX)
+a = k/L
 
 # Initialize matrix of zeroes to represent nodes
 Ts = np.zeros(Ts_dim)
@@ -34,12 +35,17 @@ Ts = np.zeros(Ts_dim)
 Ts[:,0] = 50
 Ts[ITMAX-1,:] = 50
 Ts[0,:] = 100
-print(Ts)
+#print(Ts)
 
-while np.min(np.abs()) <= R_t:
+k = 0
+conv_tol = 1
+while k < 2000 and conv_tol > R_t:
     for j in range(1,JTCV):
         for i in range(1,ITCV):
-            Ts[i,j] = (Ts[i-1,j] + Ts[i+1,j] + Ts[i,j+1] + Ts[i,j-1])/4
+            Ts[i,j] = (omega/(4*a))*(a*Ts[i-1,j] + a*Ts[i+1,j] + a*Ts[i,j+1] + a*Ts[i,j-1])
+    k+=1
+
+print(Ts)
 
 
 ###################################
@@ -53,7 +59,7 @@ Ts_2 = np.zeros(Ts_dim)
 Ts_2[:,0] = 50
 Ts_2[ITMAX-1,:] = 100
 Ts_2[0,:] = 100
-print(Ts_2)
+#print(Ts_2)
 
 
 
