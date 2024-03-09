f42c7a2c5b76c1fddfb87c5000de12eb2d00c996
c-smith01 <65824615+cole-cmd@users.noreply.github.com>
2024-03-08 22:53:42 -0600

    Finished main body of code

--- SMITH_NUEN644_HW3.py
+++ SMITH_NUEN644_HW3.py
@@ -1,7 +1,7 @@
 '''
 Created by Coleman Smith on 1/23/24
 NUEN/MEEN 644 HW1
-Due 15 February 2024
+Due 8 March 2024
 '''
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,7 +12,7 @@ k     = 386  # W/m*K
 beta  = 100  # W/m^2*C
 T_0   = 100  # deg C
 T_inf = 30   # deg C
-q_in  = 10E3 # W/m
+J     = 10E3 # kW/m -> W/m
 omega = 1.1
 R_t   = 1E-5
 
@@ -35,6 +35,8 @@ a_N = k/dely
 a_S = a_N
 a_W = k/delx
 a_E = a_W
+a_P = a_N+a_S+a_E+a_W
+b_P = -J
 
 # Initialize matrix of zeroes to represent nodes
 Ts = np.zeros(Ts_dim)
@@ -47,22 +49,16 @@ Ts[0,:] = 100
 
 v = 0
 conv_tol = 1
-while v < 2000 and conv_tol > R_t:
-    for j in range(1,JTCV):
-        for i in range(1,ITCV):
-            if j == JTCV-1:
-                Ts[i,j] = (omega/(4*a))*(a*Ts[i-1,j] + a*Ts[i+1,j] + a*Ts[i,j+1] + a*Ts[i,j-1])
-            elif i == ITCV-1:
-                Ts[i,j] = (omega/(4*a))*(a*Ts[i-1,j] + a*Ts[i+1,j] + a*Ts[i,j+1] + a*Ts[i,j-1])
-            elif i == ITCV-1 and j == JTCV-1:
-                Ts[i,j] = (omega/(4*a))*(a*Ts[i-1,j] + a*Ts[i+1,j] + a*Ts[i,j+1] + a*Ts[i,j-1])
-            elif i == ITCV-1:
-                Ts[i,j] = (omega/(4*a))*(a*Ts[i-1,j] + a*Ts[i+1,j] + a*Ts[i,j+1] + a*Ts[i,j-1])
+while v < 200 and conv_tol > R_t:
+    for j in range(1,JTCV+1):
+        for i in range(1,ITCV+1):
+            if i == ITCV:
+                Ts[i,j] = Ts[i,j] + (omega/(a_P))*(a_W*Ts[i-1,j] + a_E*Ts[i+1,j] + a_N*Ts[i,j+1] + a_S*Ts[i,j-1] - a_P*Ts[i,j] + b_P)
             else:
-                Ts[i,j] = (omega/(4*a))*(a*Ts[i-1,j] + a*Ts[i+1,j] + a*Ts[i,j+1] + a*Ts[i,j-1])
+                Ts[i,j] = Ts[i,j] + (omega/(a_P))*(a_W*Ts[i-1,j] + a_E*Ts[i+1,j] + a_N*Ts[i,j+1] + a_S*Ts[i,j-1] - a_P*Ts[i,j])
     v+=1
-
-print(Ts)
+#print(v)
+#print(Ts)
 
 
 ###################################
@@ -76,7 +72,38 @@ Ts_2 = np.zeros(Ts_dim)
 Ts_2[:,0] = 50
 Ts_2[ITMAX-1,:] = 100
 Ts_2[0,:] = 100
+
+v = 0
+conv_tol = 1
+while v < 200 and conv_tol > R_t:
+    for j in range(1,JTCV+1):
+        for i in range(1,ITCV+1):
+            if i == ITCV:
+                Ts_2[i,j] = Ts_2[i,j] + (omega/(a_P))*(a_W*Ts_2[i-1,j] + a_E*Ts_2[i+1,j] + a_N*Ts_2[i,j+1] + a_S*Ts_2[i,j-1] - a_P*Ts_2[i,j] + b_P)
+            else:
+                Ts_2[i,j] = Ts_2[i,j] + (omega/(a_P))*(a_W*Ts_2[i-1,j] + a_E*Ts_2[i+1,j] + a_N*Ts_2[i,j+1] + a_S*Ts_2[i,j-1] - a_P*Ts_2[i,j])
+    v+=1
+#print(v)
 #print(Ts_2)
+
+# Example temperature matrix (replace this with your actual temperature data)
+# Let's assume this is a 10x10 grid of temperatures
+# temperature_matrix = np.random.rand(10, 10) * 100  # Generating random temperatures between 0 and 100
+
+# # Create a meshgrid for the x and y coordinates
+# # Assuming these temperatures are measured at 1 unit apart in both x and y directions
+x = np.arange(0, Ts_2.shape[1], 1)
+y = np.arange(0, Ts_2.shape[0], 1)
+X, Y = np.meshgrid(x, y)
+
+# Plotting the contour map
+# plt.figure(figsize=(8, 6))
+# contour = plt.contourf(X, Y, Ts_2, cmap='viridis', levels=100)
+# plt.colorbar(contour)  # Add a colorbar to show the temperature scale
+# plt.title('Problem 2 Symmetric Temperature Contour Plot')
+# plt.xlabel('X Coordinate')
+# plt.ylabel('Y Coordinate')
+# plt.show()
 
 
 
@@ -84,15 +111,75 @@ Ts_2[0,:] = 100
 #########  Problem #3 #############
 ###################################
 
+N_CVs = [5,25,35,49]
+sol_storage = []
+Ts[:,0] = 50
+Ts[ITMAX-1,:] = 50
+Ts[0,:] = 100
+#print(Ts)
+
+v = 0
+conv_tol = 1
+for CVdims in N_CVs:
+
+    ITCV      = CVdims
+    JTCV      = ITCV
+    ITMAX     = ITCV + 2
+    JTMAX     = ITMAX + 2
+    Ts_dim    = (ITMAX, ITMAX)
+    capdelx   = L/ITCV
+    delx      = capdelx/2
+    capdely   = L/ITCV
+    dely      = capdely/2
+
+    Ts_3 = np.zeros(Ts_dim)
+    Ts_3[:,0] = 50
+    Ts_3[ITMAX-1,:] = 50
+    Ts_3[0,:] = 100
+
+    a_N = k/dely
+    a_S = a_N
+    a_W = k/delx
+    a_E = a_W
+    a_P = a_N+a_S+a_E+a_W
+    while v < 200 and conv_tol > R_t:
+        for j in range(1,JTCV+1):
+            for i in range(1,ITCV+1):
+                if i == ITCV:
+                    Ts_3[i,j] = Ts_3[i,j] + (omega/(a_P))*(a_W*Ts_3[i-1,j] + a_E*Ts_3[i+1,j] + a_N*Ts_3[i,j+1] + a_S*Ts_3[i,j-1] - a_P*Ts_3[i,j] + b_P)
+                else:
+                    Ts_3[i,j] = Ts_3[i,j] + (omega/(a_P))*(a_W*Ts_3[i-1,j] + a_E*Ts_3[i+1,j] + a_N*Ts_3[i,j+1] + a_S*Ts_3[i,j-1] - a_P*Ts_3[i,j])
+        v+=1
+    sol_storage.append(Ts_3)
+    #print(v)
+    #print(Ts)'
+    x = np.arange(0, Ts_3.shape[1], 1)
+    y = np.arange(0, Ts_3.shape[0], 1)
+    X, Y = np.meshgrid(x, y)
+
+    # Plotting the contour map
+    plt.figure(figsize=(8, 6))
+    contour = plt.contourf(X, Y, Ts_3, cmap='viridis', levels=100)
+    plt.colorbar(contour)  # Add a colorbar to show the temperature scale
+    plt.title(f'Problem 3 Temperature Contour Plot for a {CVdims} by {CVdims} matrix')
+    plt.xlabel('X Coordinate')
+    plt.ylabel('Y Coordinate')
+    plt.show()
 
 
 ###################################
 #########  Problem #4 #############
 ###################################
+for mtrxs in sol_storage:
 
-N_CVs = [5,25,35,49]
 
 
+plt.figure(figsize=(8, 6))
+plt.plot(N_CVs, )  # Add a colorbar to show the temperature scale
+plt.title(f'Problem 3 Temperature Contour Plot for a {CVdims} by {CVdims} matrix')
+plt.xlabel('X Coordinate')
+plt.ylabel('Y Coordinate')
+plt.show()
 
 ###################################
 #########  Problem #5 #############
