54e90e11de8ab4c9ed6b1b0081bc8d70325b023b
c-smith01 <65824615+cole-cmd@users.noreply.github.com>
2024-03-18 19:36:38 -0500

    Updated formatting

--- SMITH_NUEN644_HW4.py
+++ SMITH_NUEN644_HW4.py
@@ -12,15 +12,15 @@ os.system('clear')
 
 
 # Define constants
-L       = 1       #m
-omega   = 0.5     # Reccomended relaxation factor
-T_H2O   = 20      # Deg C
-Ru_tol  = 1E-6   # Tolerance for u-vel residual
-Rv_tol  = Ru_tol # Tolerance for v-vel residual
-R_p_tol = 1E-5  # Tolerance for Pressure residual
-Re      = 100        # Unitless Reynolds #
-rho_H2O = 998.3 # kg/m^3
-mu_H2O  = 1.002E-3 #N*s/m^2
+L       = 1        # m
+omega   = 0.5      # Reccomended relaxation factor
+T_H2O   = 20       # Deg C
+Ru_tol  = 1E-6     # Tolerance for u-vel residual
+Rv_tol  = Ru_tol   # Tolerance for v-vel residual
+Rp_tol = 1E-5      # Tolerance for Pressure residual
+Re      = 100      # Unitless Reynolds #
+rho_H2O = 998.3    # kg/m^3
+mu_H2O  = 1.002E-3 # N*s/m^2
 N_CVs   = [5,8,16,64, 128, 256]
 
 ###################################
@@ -43,7 +43,11 @@ Ts[ITMAX-1,:] = 50
 Ts[0,:] = 100
 print(Ts)
 
-while np.min(np.abs()) <= R_t:
+Ru = 1 # Start all residuals at one to force at least one iteration
+Rv = Ru
+Rp = Ru
+
+while Ru>Ru_tol and Rv>Rv_tol and Rp>Rp_tol :
     for j in range(1,JTCV):
         for i in range(1,ITCV):
             Ts[i,j] = (Ts[i-1,j] + Ts[i+1,j] + Ts[i,j+1] + Ts[i,j-1])/4
