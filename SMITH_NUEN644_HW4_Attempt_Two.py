f1ec5ef3db016a4637e0538a9e8b17f52a9df75e
c-smith01 <147568006+c-smith01@users.noreply.github.com>
2024-04-09 10:47:28 -0500

    added conditional for

--- SMITH_NUEN644_HW4_Attempt_Two.py
+++ SMITH_NUEN644_HW4_Attempt_Two.py
@@ -13,17 +13,18 @@ os.system('clear')
 
 
 # Define constants
-L       = 1                         # m
-omega   = 0.5                       # Reccomended relaxation factor
-T_H2O   = 20                        # Deg C
-Ru_tol  = 1E-6                      # Tolerance for u-vel residual
-Rv_tol  = Ru_tol                    # Tolerance for v-vel residual
-Rp_tol = 1E-5                       # Tolerance for Pressure residual
-Re      = 100                       # Unitless Reynolds #
-rho_H2O = 998.3                     # kg/m^3
-mu_H2O  = 1.002E-3                  # N*s/m^2
-u_0     = (Re*mu_H2O)/(rho_H2O*L)   # m/s
-N_CVs   = [5, 8, 16, 64, 128, 256]  # CVs
+L           = 1                         # m
+omega       = 0.5                       # Reccomended relaxation factor
+T_H2O       = 20                        # Deg C
+Ru_tol      = 1E-6                      # Tolerance for u-vel residual
+Rv_tol      = Ru_tol                    # Tolerance for v-vel residual
+Rp_tol      = 1E-5                      # Tolerance for Pressure residual
+Re          = 100                       # Unitless Reynolds #
+rho_H2O     = 998.3                     # kg/m^3
+mu_H2O      = 1.002E-3                  # N*s/m^2
+u_0         = (Re*mu_H2O)/(rho_H2O*L)   # m/s
+N_CVs_one   = [5]
+N_CVs_two   = [8, 16, 64, 128, 256]     # Dimensions of CVs
 
 # General methods used in both problems
 
@@ -37,7 +38,14 @@ def bnd_conds_two(u_matr):
 def reset(matr,dims):
     matr = np.zeros(dims)
 
-def ucoeffs(dims):
+def ucoeffs(dims,pstate):
+    if pstate == True:
+        jlim = dims
+    else:
+        jlim = dims-1
+    for j in range(1,jlim):
+        for i in range(0,dims):
+
     
 def usolve()
 
@@ -56,7 +64,10 @@ def vcorrect():
 def pcorrect():
     
 def conv_check():
-    
+    Rp = (np.sum(rho_H2O*u-rho_H2O*u,rho_H2O*u-rho_H2O*u))/(rho_H2O*u_0*L)
+    Ru = (np.sum(np.multiply(aPu*u)-np.multiply(aEu*u)-np.multiply(aWu*u)-np.multiply(aNu*u)-np.multiply(aSu*u)))/(np.sum(np.multiply(aPu*u)))
+    Rv = (np.sum(np.multiply(aPu*u)-np.multiply(aEu*u)-np.multiply(aWu*u)-np.multiply(aNu*u)-np.multiply(aSu*u)))/(np.sum(np.multiply(aPu*u)))
+
 def print_res(ps,us,vs):
     print('Printing pressure solutions')
     print(ps)
