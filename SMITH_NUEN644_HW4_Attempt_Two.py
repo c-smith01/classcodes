1d3b1e1e4ecc513757f09f369035cccbb8c88cb3
c-smith01 <65824615+cole-cmd@users.noreply.github.com>
2024-04-09 16:52:02 -0500

    added more math to SIMPLE_sol

--- SMITH_NUEN644_HW4_Attempt_Two.py
+++ SMITH_NUEN644_HW4_Attempt_Two.py
@@ -28,12 +28,12 @@ N_CVs_two   = [8, 16, 64, 128, 256]     # Dimensions of CVs
 
 # General methods used in both problems
 
-def bnd_conds_one(u_matr):
-    u_matr[:][0] = u_0
-    u_matr[:][-1] = u_0
-
-def bnd_conds_two(u_matr):
-    u_matr[:][0] = u_0
+def bnd_conds(u_matr,pstate):
+    if pstate == True:
+        u_matr[:][0] = u_0
+        u_matr[:][-1] = u_0
+    else:
+        u_matr[:][0] = u_0
 
 def reset(matr,dims):
     matr = np.zeros(dims)
@@ -43,8 +43,7 @@ def ucoeffs(dims,
             Feu,Fwu,Fnu,Fsu,
             Peu,Pwu,Pnu,Psu,
             aEu,aWu,aNu,aSu,aPu,
-            u,pstate):
-    dy = dx = L/dims
+            u,pstate,dy,dx):
     if pstate == True:
         jlim = dims
     else:
@@ -91,51 +90,49 @@ def vcoeffs(dims,
             Fev,Fwv,Fnv,Fsv,
             Peu,Pwu,Pnu,Psu,
             aEu,aWu,aNu,aSu,aPu,
-            v,pstate):
-    dy = dx = L/dims
+            v,pstate,dx,dy):
+    
 
 def vsolve(dims,v,aEv,aWv,aSv,aNv,aPv,pstate):
     
-def pcoeffs(dims,p,bP,
-            aE,aW,aN,aS,aP,
-            bPP,aEP,aWP,aNP,aSP,aPP):
-    bP[i,j] = rho_H2O*dy*(u[i-1,j]-u[i,j]) + rho_H2O*dx*(v[i,j-1]-v[i,j])
-    aE[i,j] = De[i,j]*np.max(0,(1-0.1*np.abs(Pe[i,j]))^5) + np.max(0,(-Fe[i,j]))
-    aW[i,j] = Dw[i,j]*np.max(0,(1-0.1*np.abs(Pw[i,j]))^5) + np.max(0,(-Fw[i,j]))
-    aN[i,j] = Dn[i,j]*np.max(0,(1-0.1*np.abs(Pn[i,j]))^5) + np.max(0,(-Fn[i,j]))
-    aS[i,j] = Ds[i,j]*np.max(0,(1-0.1*np.abs(Ps[i,j]))^5) + np.max(0,(-Fs[i,j]))
-    aP[i,j] = aE[i,j]+aW[i,j]+aN[i,j]+aS[i,j]
-    bPP[i,j] = rho_H2O*dy*(u[i-1,j]-u[i,j]) + rho_H2O*dx*(v[i,j-1]-v[i,j])
-    aEP[i,j] = rho_H2O*du[i,j]*dy
-    aWP[i,j] = rho_H2O*du[i,j]*dy
-    aNP[i,j] = rho_H2O*du[i,j]*dx
-    aSP[i,j] = rho_H2O*du[i,j]*dx
-    aPP[i,j] = aEP[i,j]+aWP[i,j]+aNP[i,j]+aSP[i,j]
+def pcoeffs(dims,u,v,p,
+            bPP,aEP,aWP,aNP,aSP,aPP,
+            du,dv,dx,dy,jlim):
+    for j in range(1,jlim):
+        for i in range(0,dims): 
+            bPP[i,j] = rho_H2O*dy*(u[i-1,j]-u[i,j]) + rho_H2O*dx*(v[i,j-1]-v[i,j])
+            aEP[i,j] = rho_H2O*du[i,j]*dy
+            aWP[i,j] = rho_H2O*du[i,j]*dy
+            aNP[i,j] = rho_H2O*dv[i,j]*dx
+            aSP[i,j] = rho_H2O*dv[i,j]*dx
+            aPP[i,j] = aEP[i,j]+aWP[i,j]+aNP[i,j]+aSP[i,j]
     
 def psolve(p,p_prm,aEP,aWP,aNP,aSP,aPP):
-    p_prm[i,j] = (aEP[i,j]*p[i+1,j]+aWP[i,j]*p[i-1,j]+aNP[i,j]*p[i,j+1]+aSP[i,j]*p[i,j-1])*(omega/aPP[i,j])
+    for j in range(1,jlim):
+        for i in range(0,dims):
+            p_prm[i,j] = (aEP[i,j]*p[i+1,j]+aWP[i,j]*p[i-1,j]+aNP[i,j]*p[i,j+1]+aSP[i,j]*p[i,j-1])*(omega/aPP[i,j])
     
 def ucorrect(dims,u_prm,du,p_prm):
-    if i == dims+1:
-        u_prm[i,j] = du[i,j]*(p_prm[i,j]-p_prm[i-1,j])
-        
-    else:
-        u_prm[i,j] = du[i,j]*(p_prm[i,j]-p_prm[i+1,j])
+    for j in range(1,jlim):
+        for i in range(0,dims):
+            u_prm[i,j] = du[i,j]*(p_prm[i,j]-p_prm[i-1,j])
+    u = u + u_prm
 
-def vcorrect(dims,v_prm,du,p_prm):
-    if i == dims+1:
-        v_prm[i,j] = du[i,j]*(p_prm[i,j]-p_prm[i-1,j])
-    else:
-        v_prm[i,j] = du[i,j]*(p_prm[i,j]-p_prm[i+1,j])
+def vcorrect(dims,v_prm,dv,p_prm):
+    for j in range(1,jlim):
+        for i in range(0,dims):
+            v_prm[i,j] = dv[i,j]*(p_prm[i,j]-p_prm[i-1,j])
     v = v + v_prm
 
 def pcorrect(p,p_prm):
     p = p + (omega*p_prm)
     
-def conv_check():
+def conv_check(p,u,v,
+               aEu,aWu,aNu,aSu,aPu,
+               aEv,aWv,aNv,aSv,aPv):
     Rp = (np.sum(rho_H2O*u-rho_H2O*u,rho_H2O*u-rho_H2O*u))/(rho_H2O*u_0*L)
     Ru = (np.sum(np.multiply(aPu*u)-np.multiply(aEu*u)-np.multiply(aWu*u)-np.multiply(aNu*u)-np.multiply(aSu*u)))/(np.sum(np.multiply(aPu*u)))
-    Rv = (np.sum(np.multiply(aPu*u)-np.multiply(aEu*u)-np.multiply(aWu*u)-np.multiply(aNu*u)-np.multiply(aSu*u)))/(np.sum(np.multiply(aPu*u)))
+    Rv = (np.sum(np.multiply(aPv*u)-np.multiply(aEv*u)-np.multiply(aWv*u)-np.multiply(aNv*u)-np.multiply(aSv*u)))/(np.sum(np.multiply(aPu*u)))
 
 def print_res(ps,us,vs):
     print('Printing pressure solutions')
@@ -155,17 +152,36 @@ P1_psols = []
 P1_usols = []
 P1_vsols = [] # initialize empty array to contain solutions for post-processing
 
-def SIMPLE_sol(cv_arr,iter_lim):
-    pstate = True #let dependent methods know this is Problem 1
+def SIMPLE_sol(cv_arr,iter_lim,pstate):
     
     for dims in cv_arr:
+        #init geometry
+        dx = dy = L/dims
         bnd_size = (dims,dims)
-        p = np.ones(bnd_size)
-        v = np.ones(bnd_size)
-        u = np.ones(bnd_size)
-        p_prm = np.ones(bnd_size)
-        v_prm = np.ones(bnd_size)
-        u_prm = np.ones(bnd_size)
+
+        #init necessary matrices
+        p = v = u = np.ones(bnd_size)
+        p_prm = v_prm = u_prm = np.ones(bnd_size)
+        dv = du = np.ones(bnd_size)
+        Deu = Dwu = Dnu = Dsu = Dev = Dwv = Dnv = Dsv = np.ones(bnd_size)
+        Feu = Fwu = Fnu = Fsu = Fev = Fwv = Fnv = Fsv = np.ones(bnd_size)
+        Peu = Pwu = Pnu = Psu = Pev = Pwv = Pnv = Psv = np.ones(bnd_size)
+        Peu = Pwu = Pnu = Psu = Pev = Pwv = Pnv = Psv = np.ones(bnd_size)
+        aEu = aWu = aNu = aSu = aPu = aEu = aWu = aNu = aSu = aPu = np.ones(bnd_size)
+        aEP = aWP = aNP = aSP = aPP = bPP = np.ones(bnd_size)
+
+        Rp = Ru = Rv = 1 #start residuals with values greater than tolerance to force at least one iteration
+        itercount = 1 # start count of iterations to convergence
+
+        while Rp>Rp_tol or Ru>Ru_tol or Rv>Rv_tol or itercount<iter_lim:
+            bnd_conds(u_matr=u,pstate=pstate)
+
+            
+
+
+        print_res()
+
+
 
 
 
