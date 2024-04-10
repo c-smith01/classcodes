678b8add74cbecb43734317fcbaad0be3c2ce4be
c-smith01 <147568006+c-smith01@users.noreply.github.com>
2024-04-09 19:42:52 -0500

    debugging

--- SMITH_NUEN644_HW4_Attempt_Two.py
+++ SMITH_NUEN644_HW4_Attempt_Two.py
@@ -71,12 +71,12 @@ def ucoeffs(dims,
                 Dsu[i,j] = mu_H2O*dx/dy
 
             # Calculate flow strengths
-            if i == 0:
-                Feu[i,j] = rho_H2O*(0.5*(u[i,j] + u[i+1,j]))*dy
-            else:    
+            if i == dims-1:
+                Feu[i,j] = 0
+            else:
                 Feu[i,j] = rho_H2O*(0.5*(u[i,j] + u[i+1,j]))*dy
 
-            if i == dims-1:
+            if i == 0:
                 Fwu[i,j] = 0
             else:
                 Fwu[i,j] = rho_H2O*(0.5*(u[i,j] + u[i-1,j]))*dy
@@ -112,28 +112,43 @@ def ucoeffs(dims,
             else:
                 Psu[i,j] = Fwu[i,j]/Dwu[i,j]
 
-            aEu[i,j] = Deu[i,j]*np.max(0,(1-0.1*np.abs(Peu[i,j]))^5) + np.max(0,(-Feu[i,j]))
-            aWu[i,j] = Dwu[i,j]*np.max(0,(1-0.1*np.abs(Pwu[i,j]))^5) + np.max(0,(-Fwu[i,j]))
-            aNu[i,j] = Dnu[i,j]*np.max(0,(1-0.1*np.abs(Pnu[i,j]))^5) + np.max(0,(-Fnu[i,j]))
-            aSu[i,j] = Dsu[i,j]*np.max(0,(1-0.1*np.abs(Psu[i,j]))^5) + np.max(0,(-Fsu[i,j]))
+            aEu[i,j] = Deu[i,j]*np.max((0,(1-0.1*np.abs(Peu[i,j]))**5)) + np.max((0,(-Feu[i,j])))
+            aWu[i,j] = Dwu[i,j]*np.max((0,(1-0.1*np.abs(Pwu[i,j]))**5)) + np.max((0,(-Fwu[i,j])))
+            aNu[i,j] = Dnu[i,j]*np.max((0,(1-0.1*np.abs(Pnu[i,j]))**5)) + np.max((0,(-Fnu[i,j])))
+            aSu[i,j] = Dsu[i,j]*np.max((0,(1-0.1*np.abs(Psu[i,j]))**5)) + np.max((0,(-Fsu[i,j])))
             aPu[i,j] = aEu[i,j]+aWu[i,j]+aNu[i,j]+aSu[i,j]
     
 def usolve(dims,
-           u,aEu,aWu,aSu,aNu,aPu,pstate):
+           u,aEu,aWu,aSu,aNu,aPu,
+           du,dy,pstate):
     if pstate == True:
         jlim = dims
     else:
         jlim = dims-1
     for j in range(1,jlim):
         for i in range(0,dims):
+            if i == 0:
+                u[i,j] = (aEu[i,j]*u[i+1,j]+aNu[i,j]*u[i,j+1]+aSu[i,j]*u[i,j-1])*(omega/aPu[i,j])
+                du[i,j] = dy/((aPu[i,j]/omega)-(aEu[i,j]+aWu[i,j]+aNu[i,j]+aSu[i,j]))
+            elif i == dims-1:
+                u[i,j] = (aWu[i,j]*u[i-1,j]+aNu[i,j]*u[i,j+1]+aSu[i,j]*u[i,j-1])*(omega/aPu[i,j])
+                du[i,j] = dy/((aPu[i,j]/omega)-(aEu[i,j]+aWu[i,j]+aNu[i,j]+aSu[i,j]))
+            elif j == 0:
+                u[i,j] = (aWu[i,j]*u[i-1,j]+aNu[i,j]*u[i,j+1])*(omega/aPu[i,j])
+                du[i,j] = dy/((aPu[i,j]/omega)-(aEu[i,j]+aWu[i,j]+aNu[i,j]+aSu[i,j]))
+            elif j == dims-1:
+                u[i,j] = (aEu[i,j]*u[i+1,j]+aWu[i,j]*u[i-1,j]+aSu[i,j]*u[i,j-1])*(omega/aPu[i,j])
+                du[i,j] = dy/((aPu[i,j]/omega)-(aEu[i,j]+aWu[i,j]+aNu[i,j]+aSu[i,j]))
+            else:
                 u[i,j] = (aEu[i,j]*u[i+1,j]+aWu[i,j]*u[i-1,j]+aNu[i,j]*u[i,j+1]+aSu[i,j]*u[i,j-1])*(omega/aPu[i,j])
+                du[i,j] = dy/((aPu[i,j]/omega)-(aEu[i,j]+aWu[i,j]+aNu[i,j]+aSu[i,j]))
 
 def vcoeffs(dims,
             Dev,Dwv,Dnv,Dsv,
             Fev,Fwv,Fnv,Fsv,
             Pev,Pwv,Pnv,Psv,
             aEv,aWv,aNv,aSv,aPv,
-            v,pstate,dx,dy):
+            v,dx,dy):
     for j in range(0,dims):
         for i in range(0,dims):
             # Calculate diffusion strengths
@@ -199,23 +214,38 @@ def vcoeffs(dims,
             else:
                 Psv[i,j] = Fwv[i,j]/Dwv[i,j]
 
-            aEv[i,j] = Dev[i,j]*np.max(0,(1-0.1*np.abs(Pev[i,j]))^5) + np.max(0,(-Fev[i,j]))
-            aWv[i,j] = Dwv[i,j]*np.max(0,(1-0.1*np.abs(Pwv[i,j]))^5) + np.max(0,(-Fwv[i,j]))
-            aNv[i,j] = Dnv[i,j]*np.max(0,(1-0.1*np.abs(Pnv[i,j]))^5) + np.max(0,(-Fnv[i,j]))
-            aSv[i,j] = Dsv[i,j]*np.max(0,(1-0.1*np.abs(Psv[i,j]))^5) + np.max(0,(-Fsv[i,j]))
+            aEv[i,j] = Dev[i,j]*np.max((0,(1-0.1*np.abs(Pev[i,j]))**5)) + np.max((0,(-Fev[i,j])))
+            aWv[i,j] = Dwv[i,j]*np.max((0,(1-0.1*np.abs(Pwv[i,j]))**5)) + np.max((0,(-Fwv[i,j])))
+            aNv[i,j] = Dnv[i,j]*np.max((0,(1-0.1*np.abs(Pnv[i,j]))**5)) + np.max((0,(-Fnv[i,j])))
+            aSv[i,j] = Dsv[i,j]*np.max((0,(1-0.1*np.abs(Psv[i,j]))**5)) + np.max((0,(-Fsv[i,j])))
             aPv[i,j] = aEv[i,j]+aWv[i,j]+aNv[i,j]+aSv[i,j]    
 
 def vsolve(dims,
-           v,aEv,aWv,aSv,aNv,aPv,pstate):
+           v,aEv,aWv,aSv,aNv,aPv,
+           dv,dx):
     for i in range(0,dims):
         for j in range(dims):
-            v[i,j] = (aEv[i,j]*v[i+1,j]+aWv[i,j]*v[i-1,j]+aNv[i,j]*v[i,j+1]+aSv[i,j]*v[i,j-1])*(omega/aPv[i,j])
+            if i == 0:
+                v[i,j] = (aEv[i,j]*v[i+1,j]+aWv[i,j]*v[i-1,j]+aNv[i,j]*v[i,j+1]+aSv[i,j]*v[i,j-1])*(omega/aPv[i,j])
+                dv[i,j] = dx/((aPv[i,j]/omega)-(aEv[i,j]+aWv[i,j]+aNv[i,j]+aSv[i,j]))
+            elif i == dims-1:
+                v[i,j] = (aEv[i,j]*v[i+1,j]+aWv[i,j]*v[i-1,j]+aNv[i,j]*v[i,j+1]+aSv[i,j]*v[i,j-1])*(omega/aPv[i,j])
+                dv[i,j] = dx/((aPv[i,j]/omega)-(aEv[i,j]+aWv[i,j]+aNv[i,j]+aSv[i,j]))
+            elif j == 0:
+                v[i,j] = (aEv[i,j]*v[i+1,j]+aWv[i,j]*v[i-1,j]+aNv[i,j]*v[i,j+1]+aSv[i,j]*v[i,j-1])*(omega/aPv[i,j])
+                dv[i,j] = dx/((aPv[i,j]/omega)-(aEv[i,j]+aWv[i,j]+aNv[i,j]+aSv[i,j]))
+            elif j == dims-1:
+                v[i,j] = (aEv[i,j]*v[i+1,j]+aWv[i,j]*v[i-1,j]+aNv[i,j]*v[i,j+1]+aSv[i,j]*v[i,j-1])*(omega/aPv[i,j])
+                dv[i,j] = dx/((aPv[i,j]/omega)-(aEv[i,j]+aWv[i,j]+aNv[i,j]+aSv[i,j]))
+            else:
+                v[i,j] = (aEv[i,j]*v[i+1,j]+aWv[i,j]*v[i-1,j]+aNv[i,j]*v[i,j+1]+aSv[i,j]*v[i,j-1])*(omega/aPv[i,j])
+                dv[i,j] = dx/((aPv[i,j]/omega)-(aEv[i,j]+aWv[i,j]+aNv[i,j]+aSv[i,j]))
     
 def pccoeffs(dims,
              u,v,
-            bPP,aEP,aWP,aNP,aSP,aPP,
-            du,dv,dx,dy,jlim):
-    for j in range(1,jlim):
+             bPP,aEP,aWP,aNP,aSP,aPP,
+             du,dv,dx,dy):
+    for j in range(0,dims):
         for i in range(0,dims): 
             bPP[i,j] = rho_H2O*dy*(u[i-1,j]-u[i,j]) + rho_H2O*dx*(v[i,j-1]-v[i,j])
             aEP[i,j] = rho_H2O*du[i,j]*dy
@@ -226,20 +256,20 @@ def pccoeffs(dims,
     
 def pcsolve(dims,
             p,p_prm,aEP,aWP,aNP,aSP,aPP):
-    for j in range(1,jlim):
+    for j in range(0,dims):
         for i in range(0,dims):
             p_prm[i,j] = (aEP[i,j]*p[i+1,j]+aWP[i,j]*p[i-1,j]+aNP[i,j]*p[i,j+1]+aSP[i,j]*p[i,j-1])*(omega/aPP[i,j])
     
 def ucorrect(dims,
              u_prm,du,p_prm):
-    for j in range(1,jlim):
+    for j in range(0,dims):
         for i in range(0,dims):
             u_prm[i,j] = du[i,j]*(p_prm[i,j]-p_prm[i-1,j])
     u = u + u_prm
 
 def vcorrect(dims,
              v_prm,dv,p_prm):
-    for j in range(1,jlim):
+    for j in range(0,dims):
         for i in range(0,dims):
             v_prm[i,j] = dv[i,j]*(p_prm[i,j]-p_prm[i-1,j])
     v = v + v_prm
@@ -250,17 +280,21 @@ def pcorrect(p,p_prm):
 def conv_check(p,u,v,
                aEu,aWu,aNu,aSu,aPu,
                aEv,aWv,aNv,aSv,aPv):
-    Rp = (np.sum(rho_H2O*u-rho_H2O*u,rho_H2O*u-rho_H2O*u))/(rho_H2O*u_0*L)
     Ru = (np.sum(np.multiply(aPu*u)-np.multiply(aEu*u)-np.multiply(aWu*u)-np.multiply(aNu*u)-np.multiply(aSu*u)))/(np.sum(np.multiply(aPu*u)))
-    Rv = (np.sum(np.multiply(aPv*u)-np.multiply(aEv*u)-np.multiply(aWv*u)-np.multiply(aNv*u)-np.multiply(aSv*u)))/(np.sum(np.multiply(aPu*u)))
 
-def print_res(ps,us,vs):
+    Rv = (np.sum(np.multiply(aPv*v)-np.multiply(aEv*v)-np.multiply(aWv*v)-np.multiply(aNv*v)-np.multiply(aSv*v)))/(np.sum(np.multiply(aPv*v)))
+
+    Rp = (np.sum(rho_H2O*u-rho_H2O*u,rho_H2O*u-rho_H2O*u))/(rho_H2O*u_0*L)
+    return Rp,Ru,Rv
+
+def print_res(p,u,v,itercount,Rp,Ru,Rv):
+    print('SIMPLE Algorithm terminated at {} iterations with Rp = {}, Ru = {}, Rv = {}'.format(itercount,Rp,Ru,Rv))
     print('Printing pressure solutions')
-    print(ps)
+    print(p)
     print('Printing u-vel fields')
-    print(us)
+    print(u)
     print('Printing v-vel fields')
-    print(vs)
+    print(v)
 
 
 
@@ -271,6 +305,11 @@ def print_res(ps,us,vs):
 def SIMPLE_sol(cv_arr,iter_lim,pstate):
     
     for dims in cv_arr:
+        #init solutions arrays
+        psols = []
+        usols = []
+        vsols = []
+
         #init geometry
         dx = dy = L/dims
         cv_size = dims+2
@@ -284,7 +323,7 @@ def SIMPLE_sol(cv_arr,iter_lim,pstate):
         Feu = Fwu = Fnu = Fsu = Fev = Fwv = Fnv = Fsv = np.ones(bnd_size)
         Peu = Pwu = Pnu = Psu = Pev = Pwv = Pnv = Psv = np.ones(bnd_size)
         Peu = Pwu = Pnu = Psu = Pev = Pwv = Pnv = Psv = np.ones(bnd_size)
-        aEu = aWu = aNu = aSu = aPu = aEu = aWu = aNu = aSu = aPu = np.ones(bnd_size)
+        aEu = aWu = aNu = aSu = aPu = aEv = aWv = aNv = aSv = aPv = np.ones(bnd_size)
         aEP = aWP = aNP = aSP = aPP = bPP = np.ones(bnd_size)
 
         Rp = Ru = Rv = 1 # start residuals with values greater than tolerance to force at least one iteration
@@ -295,25 +334,49 @@ def SIMPLE_sol(cv_arr,iter_lim,pstate):
             reset(matr=p_prm,dims=cv_size)
             reset(matr=u_prm,dims=cv_size)
             reset(matr=v_prm,dims=cv_size)
-            ucoeffs(Deu=Deu,Dwu=Dwu,Dnu=Dnu,Dsu=Dsu,
+            ucoeffs(dims=cv_size,
+                    Deu=Deu,Dwu=Dwu,Dnu=Dnu,Dsu=Dsu,
                     Feu=Feu,Fwu=Fwu,Fnu=Fnu,Fsu=Fsu,
                     Peu=Peu,Pwu=Pwu,Pnu=Pnu,Psu=Psu,
                     aEu=aEu,aWu=aWu,aNu=aNu,aSu=aSu,aPu=aPu,
                     u=u,pstate=pstate,dy=dy,dx=dy)
-            usolve()
-            vcoeffs()
-            vsolve()
-            pccoeffs()
-            pcsolve()
+            usolve(dims=cv_size,
+                   u=u,aEu=aEu,aWu=aWu,aSu=aSu,aNu=aNu,aPu=aPu,
+                   du=du,dy=dy,pstate=pstate)
+            vcoeffs(dims=cv_size,
+                    Dev=Dev,Dwv=Dwv,Dnv=Dnv,Dsv=Dsv,
+                    Fev=Fev,Fwv=Fwv,Fnv=Fnv,Fsv=Fsv,
+                    Pev=Pev,Pwv=Pwv,Pnv=Pnv,Psv=Psv,
+                    aEv=aEv,aWv=aWv,aNv=aNv,aSv=aSv,aPv=aPv,
+                    v=v,dx=dx,dy=dy)
+            vsolve(dims=cv_size,
+                   v=v,aEv=aEv,aWv=aWv,aSv=aSv,aNv=aNv,aPv=aPv,
+                   dv=dv,dx=dx)
+            pccoeffs(dims=cv_size,
+                     u=u,v=v,
+                     bPP=bPP,aEP=aEP,aWP=aWP,aNP=aNP,aSP=aSP,aPP=aPP,
+                     du=du,dv=dv,dx=dx,dy=dy)
+            pcsolve(dims=cv_size,
+                    p=p,p_prm=p_prm,aEP=aEP,aWP=aWP,aNP=aNP,aSP=aSP,aPP=aPP)
+            ucorrect(dims=cv_size,
+                     u_prm=u_prm,du=du,p_prm=p_prm)
+            vcorrect(dims=cv_size,
+                     v_prm=v_prm,dv=dv,p_prm=p_prm)
+            pcorrect(p=p,p_prm=p_prm)
+            Rp,Ru,Rv = conv_check(p=p,u=u,v=v,
+                                  aEu=aEu,aWu=aWu,aNu=aNu,aSu=aSu,aPu=aPu,
+                                  aEv=aEv,aWv=aWv,aNv=aNv,aSv=aSv,aPv=aPv)
 
+            itercount +=1 # add one to iteration tally
 
+        print_res(p=p,u=u,v=v,itercount=itercount,Rp=Rp,Ru=Ru,Rv=Rv)
+        psols.append(p)
+        usols.append(u)
+        vsols.append(v)
 
+    return [psols,usols,vsols]
 
-        print_res()
-
-
-
-
+prob_one_sol = SIMPLE_sol(cv_arr=N_CVs_one,iter_lim=5,pstate=True)
 
 ###################################
 #########  Problem #2 #############
