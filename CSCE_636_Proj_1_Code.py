1f3bb30769c9e63a4787acc839bfbf200be1a501
c-smith <c-smith@tamu.edu>
2025-06-15 22:09:17 -0500

    Open data files as binary. Begin model outline.

--- CSCE_636_Proj_1_Code.py
+++ CSCE_636_Proj_1_Code.py
@@ -5,12 +5,12 @@ from tensorflow import keras
 import pickle
 
 # Accessing provided data
-with open('CSCE_636_Proj_Files/DS-14-samples_n_k_m_P.pkl') as file:
+with open('CSCE_636_Proj_Files/DS-14-samples_n_k_m_P', 'rb') as file:
     loaded_input_features = pickle.load(file)
     
 print(loaded_input_features)
 
-with open('CSCE_636_Proj_Files/DS-14-samples_n_k_m_P.pkl') as file:
+with open('CSCE_636_Proj_Files/DS-14-samples_mHeights.pkl', 'rb') as file:
     loaded_output_features = pickle.load(file)
     
 print(loaded_output_features)
@@ -19,6 +19,11 @@ print(loaded_output_features)
 train_data = []
 test_data = []
 validation_data = []
+
+# initial model
+input_size=6
+output_size=1
+
 
 
 
